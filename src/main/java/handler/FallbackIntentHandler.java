23ee6349ed2c37b77d107325f25889391f7d3c41
Jason Davies <jason.davies@healthcaregateway.co.uk>
2019-10-06 16:10:17 +0100

    Added basic Alexa Handlers

---
+++ src/main/java/handler/FallbackIntentHandler.java
@@ -0,0 +1,26 @@
+package handler;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+
+import java.util.Optional;
+
+import static com.amazon.ask.request.Predicates.intentName;
+
+public class FallbackIntentHandler implements RequestHandler {
+
+    @Override
+    public boolean canHandle(HandlerInput input) {
+        return input.matches(intentName("AMAZON.FallbackIntent").or(intentName("AMAZON.CancelIntent")));
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput input) {
+        String speechText = "Bye Bye";
+        return input.getResponseBuilder()
+                .withSpeech(speechText)
+                .withSimpleCard("HelloWorld", speechText)
+                .build();
+    }
+}
